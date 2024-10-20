5e89a773970d93085f72ee4bb3fe69db6d00be15
Svyatoslav Milkin <svyatoslavmilkin@yandex.ru>
2024-10-20 08:41:07 +0300

    Added new generators for open api

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
     id("io.gitlab.arturbosch.detekt") version "1.23.7"
+    id("org.openapi.generator") version "7.8.0"
 }
 
 group = "SakiraBusinessLabs"
@@ -32,6 +35,18 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // https://mvnrepository.com/artifact/jakarta.validation/jakarta.validation-api
+    implementation("jakarta.validation:jakarta.validation-api:3.1.0")
+
+    // https://mvnrepository.com/artifact/io.swagger.core.v3/swagger-models
+    implementation("io.swagger.core.v3:swagger-models:2.2.23")
+    // https://mvnrepository.com/artifact/io.swagger.core.v3/swagger-annotations
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.23")
+
+    // https://mvnrepository.com/artifact/org.springframework/spring-webflux
+    implementation("org.springframework:spring-webflux")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 }
 
 kotlin {
@@ -49,5 +64,76 @@ configurations.all {
         if (requested.group == "org.jetbrains.kotlin") {
             useVersion(io.gitlab.arturbosch.detekt.getSupportedKotlinVersion())
         }
+    }
+}
+
+
+var buildDir: File = project.layout.buildDirectory.get().asFile
+val outputDirPath = "$buildDir/generated"
+
+// root points of openApiSpec
+val openApiSpecs = listOf(
+    "$rootDir/src/main/resources/openapi/v1/tournament.yaml",
+)
+
+openApiSpecs.forEachIndexed { index, openApiSpec ->
+    val openApiGenerateInterface = tasks.register<GenerateTask>("OpenApiGenerateInterface$index") {
+        generatorName.set("kotlin-spring")
+        inputSpec.set(openApiSpec)
+        outputDir.set(outputDirPath)
+        apiPackage.set("ru.tournament.api")
+        modelPackage.set("ru.tournament.model")
+
+        generateApiTests.set(false)
+        generateModelTests.set(false)
+        generateApiDocumentation.set(false)
+        generateModelDocumentation.set(false)
+
+        configOptions.putAll(
+            mapOf(
+                "interfaceOnly" to "true",
+                "dateLibrary" to "java8",
+                "serializationLibrary" to "jackson",
+                "useSpringBoot3" to "true",
+                "useTags" to "true",
+                "requestMappingMode" to "api_interface"
+            )
+        )
+    }
+
+    val openApiGenerateClient = tasks.register<GenerateTask>("OpenApiGenerateClient$index") {
+        generatorName.set("kotlin")
+        inputSpec.set(openApiSpec)
+        outputDir.set(outputDirPath)
+        apiPackage.set("ru.tournament.client")
+        modelPackage.set("ru.tournament.model")
+
+        generateApiTests.set(false)
+        generateModelTests.set(false)
+        generateApiDocumentation.set(false)
+        generateModelDocumentation.set(false)
+
+        configOptions.putAll(
+            mapOf(
+                "useTags" to "false",
+                "library" to "jvm-spring-webclient",
+                "dateLibrary" to "java8",
+                "serializationLibrary" to "jackson",
+                "useSpringBoot3" to "true",
+                "apiSuffix" to "StorageApiClient",
+            )
+        )
+    }
+
+    tasks.named("compileKotlin") {
+        dependsOn(openApiGenerateInterface)
+        dependsOn(openApiGenerateClient)
+    }
+}
+
+
+sourceSets.main {
+    java {
+        srcDir("$buildDir/generated/src/main/kotlin")
     }
 }
